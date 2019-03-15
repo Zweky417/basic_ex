ba33828be33ae193d64a8534ed64740f90c6d1e8
unknown <33580@ETECAF.LOCAL>
2019-03-15 17:51:38 -0300

    segundo commit

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour 
+{
+    int i = 2;
+    double d = 3.5;
+    string s = "abc";
+
+    void Start()
+    {
+        Func(1,0.5,"def");
+    }
+
+    void Func(int j, double e, string t)
+    {
+        Debug.Log(i + j);
+        Debug.Log(d + e);
+        Debug.Log(s + t);
+    }
+}
---
+++ Assets/Scripts/Main2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Main2 : MonoBehaviour
+{
+	void Start () 
+    {
+        Seila(12, 20, 8);
+	}
+
+    void Seila(double CustoRefeicao, int percGorgeta, int percImposto)
+    {
+        double Gorgeta = (int) CustoRefeicao * ((double) percGorgeta / 100);
+        double Imposto = (int) CustoRefeicao * ((double) percImposto / 100);
+
+        double CustoTotal = CustoRefeicao + Gorgeta + Imposto;
+        Debug.Log(Math.Round(CustoTotal));
+    }
+}
